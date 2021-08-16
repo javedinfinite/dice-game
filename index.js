5d1aa7567093a0253f851f5e37bddabf64964da7
Javed Akhtar <javed.akhtar@datagrokr.com>
2021-08-16 16:05:35 +0530

    refactored code

--- index.js
+++ index.js
@@ -9,36 +9,32 @@ for (let i = 0; i < noPlayers; i++) {
     players.push({name:"Player-"+i, order:i, points:0, rank:0, penalty:0})
 }
 
-const roleTheDice = (i) =>{
+const roleTheDice = (playerId) =>{
 
-    reader.question(`Player-${i} its your turn, press enter to roll the dice: `);
+    reader.question(`Player-${playerId} its your turn, press enter to roll the dice: `);
     const rndInt = Math.floor(Math.random() * 6) + 1
     console.log("dice value",rndInt)
     return rndInt
 }
 
-const play = (i) =>{
-    let diceValue = roleTheDice(i)
-    players[i].points = players[i].points + diceValue
-    if(diceValue==1)
-        players[i].penalty =  players[i].penalty + 1
-    else
-        players[i].penalty =  0
+const play = (playerId) =>{
 
-    if(diceValue==6 && players[i].points<targetPoints) 
-        play(i)
+    let diceValue = roleTheDice(playerId)
+    players[playerId].points = players[playerId].points + diceValue
+    diceValue==1? players[playerId].penalty =  players[playerId].penalty + 1 : players[playerId].penalty =  0
+        
+    if(diceValue==6 && players[playerId].points<targetPoints) 
+        play(playerId)
     
 }
 
 while(rank<noPlayers){
 
     for (let i = 0; i < noPlayers; i++) {
-        if(players[i].points<targetPoints){
-            if(players[i].penalty==2)
-                players[i].penalty = 0
-            else
-                play(i)
-        }
+
+        if(players[i].points<targetPoints)
+            players[i].penalty==2? players[i].penalty = 0 : play(i)
+       
         else if(players[i].points>=targetPoints && players[i].rank==0){
             rank +=1
             players[i].rank = rank
@@ -49,6 +45,7 @@ while(rank<noPlayers){
     console.log("Current rank table: ",players)
 
 }
+
 
 
 
