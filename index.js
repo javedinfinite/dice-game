76af1173ea1c888f6d350e6cb3cd8e38fdf41b2f
Javed Akhtar <javed.akhtar@datagrokr.com>
2021-08-21 15:58:00 +0530

    added prime bonus feature to the game

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 const reader = require("readline-sync");
 
 const players = []
+
 let noPlayers = reader.question("Number of Player: ");
 const targetPoints = reader.question("Target Points: ");
 console.log("\n")
 
-let rank = 0
+
+
+let rank = 0 //1 2 3
 let roundCounter = 1
 
 for (let i = 0; i < noPlayers; i++) {
-    players.push({name:"Player-"+i, order:i, points:0, rank:0, penalty:0})
+    players.push({name:"Player-"+i, points:0, rank:0, prime:[-1, -1, -1], penalty:0})
 }
 
 const rollTheDice = (playerId) =>{
@@ -21,10 +24,59 @@ const rollTheDice = (playerId) =>{
     return rndInt
 }
 
+function isPrime(num) {
+    for(var i = 2; i < num; i++)
+      if(num % i === 0) return false;
+    return num > 1;
+  }
+
+const primeFeature = (playerId, diceValue) =>{
+
+    for (let i = 0; i < 3; i++) {
+
+        if(players[playerId].prime[i]==-1){
+            players[playerId].prime[i] = diceValue
+            return
+        }
+            
+    }
+
+
+    players[playerId].prime.shift()
+    players[playerId].prime.push(diceValue)
+    
+    let sum = players[playerId].prime.reduce((acc, element)=>{
+        return acc + element 
+    },0)
+
+    // const primeResult = isPrime(sum)
+    const primeResult = true
+
+    if(primeResult)
+    {
+        
+        players[playerId].points += 5 
+        //iterate throuth all remaining players who is still in game and subtract 5 points
+
+        players.forEach(element => {
+            if(element.name!= 'Player-'+playerId){
+                element.points -= 5
+                if(element.points<0)
+                    element.points = 0
+            }
+                
+        });
+        console.log(`Hurrah!...The Player-${playerId} got primeBonus######################################`)
+    }
+
+
+}
+
 const play = (playerId) =>{
 
     let diceValue = rollTheDice(playerId)
     players[playerId].points = players[playerId].points + diceValue
+    primeFeature(playerId, diceValue)
     diceValue==1? players[playerId].penalty =  players[playerId].penalty + 1 : players[playerId].penalty =  0
 
     if(players[playerId].points>=targetPoints){
---
+++ test2.js
@@ -0,0 +1,23 @@
+// {name:"Player-"+i, points:0, rank:0, prime:[-1, -1, -1], penalty:0}
+
+//playersInfo
+// auto id, uuid player-Id, string Player-Name, 
+
+
+//rankTable
+//int playerId, globalRank, noOfGamesPlayed, string gamesPlayed (comma separated game IDs) 
+
+//gameInfo
+// gameId, targetPoints, rules, 
+
+//gamePlay (2, all players in g 2)
+//game-Id(non-prime-repeat), player-Id, int points, int rank, string prime, int penalty
+// 1, A, 0, ....
+// 1, B, 0, ....
+
+
+
+
+//A, B, (g-1) 
+//C, D (g-2)
+//
